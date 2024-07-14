f0af1f2ea4d16f89b89fedf669c88514b8739f85
Kruti292-web <krutisaliya02@gmail.com>
2024-07-14 09:08:45 +0530

    create sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
+
 import { Link, useLocation } from "react-router-dom";
 import {AiOutlineSearch} from "react-icons/ai"
 import {FaMoon} from "react-icons/fa"
@@ -6,8 +7,8 @@ import {FaMoon} from "react-icons/fa"
 export default function Header() {
     const path = useLocation().pathname;
   return (
-    <Navbar className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <Link to='/'>
+    <Navbar className="border-b-2">
+        <Link to='/' className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">PublisherPro</span>
             Blog
         </Link>
