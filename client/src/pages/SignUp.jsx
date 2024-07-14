67303ace671318aed6ac901bfabe402dc190b0cf
Kruti292-web <krutisaliya02@gmail.com>
2024-07-14 10:10:56 +0530

    add functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,46 @@
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] =useState({});
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e)=>{
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  } 
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(!formData.username || !formData.email  || !formData.password){
+      setErrorMessages('Please enter all fields');
+      return;
+    }
+    try {
+      setLoading(true);
+      setErrorMessages(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        setErrorMessages(data.message);
+        return;
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessages(error.message); 
+      setLoading(false);
+    }
+  };
+  
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -17,37 +55,49 @@ export default function SignUp() {
             </span>
             Blog
           </Link>
-          <p className="text-m mt-5">Publish your passions, your way
+          <p className="text-m mt-5"><strong>Publish your passions</strong>, your way
           To create a unique and beautiful blog easily, sign up with your email and password or with Google</p>
         </div>
         {/*right*/}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value='Your Username'/>
               <TextInput
                 type="text"
                 placeholder="Username"
                 id="username"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your Email'/>
               <TextInput
-                type="text"
+                type="email"
                 placeholder="name@company.com"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your Password'/>
               <TextInput
-                type="text"
+                type="password"
                 placeholder="Password"
                 id="password"
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading? (
+                  <>
+                  <Spinner size="sm"/>
+                  <span className="pl-3">Loading...</span>
+                  </>
+                ) : 'Sign Up'
+              }
+            </Button>
           </form>
           <div className="flex gap-2 mt-5 text-sm">
             <span>Have an account?</span>
@@ -55,6 +105,13 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {
+            errorMessages && (
+              <Alert className="mt-5" color="failure" >
+                {errorMessages}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
