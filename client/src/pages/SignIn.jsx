98482a725943286826c7cc14277b34c10a12e4dd
Kruti292-web <krutisaliya02@gmail.com>
2024-07-15 17:51:13 +0530

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,110 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
 export default function SignIn() {
+  const [formData, setFormData] =useState({});
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e)=>{
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  } 
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(!formData.email  || !formData.password){
+      setErrorMessages('Please enter all fields');
+      return;
+    }
+    try {
+      setLoading(true);
+      setErrorMessages(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        setErrorMessages(data.message);
+        return;
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessages(error.message); 
+      setLoading(false);
+    }
+  };
+  
   return (
-    <div>
-        SignIn      
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-7">
+        {/*left*/}
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="font-bold dark:text-white text-4xl"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              PublisherPro
+            </span>
+            Blog
+          </Link>
+          <p className="text-m mt-5"><strong>Publish your passions</strong>, your way
+          To create a unique and beautiful blog easily, sign in with your email and password or with Google</p>
+        </div>
+        {/*right*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your Email'/>
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Your Password'/>
+              <TextInput
+                type="password"
+                placeholder="***********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading? (
+                  <>
+                  <Spinner size="sm"/>
+                  <span className="pl-3">Loading...</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-5 text-sm">
+            <span>Dont have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {
+            errorMessages && (
+              <Alert className="mt-5" color="failure" >
+                {errorMessages}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
