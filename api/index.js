05df0cfaa53f3b8434b314ab033531cc26a6bdcb
Kruti292-web <krutisaliya02@gmail.com>
2024-08-30 19:47:49 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from './routes/post.route.js';
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -23,6 +24,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api/post', postRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`)
