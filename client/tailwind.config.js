24f1fe135cbb2aeb7d4f9051a913b3f28a022f79
Kruti292-web <krutisaliya02@gmail.com>
2024-08-30 21:17:50 +0530

    show user post inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,17 @@
-import flowbite from "flowbite-react/tailwind";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
+  import flowbite from "flowbite-react/tailwind";
+  import scrollbar from "tailwind-scrollbar";
+  /** @type {import('tailwindcss').Config} */
+  export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      flowbite.content(),
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      flowbite.plugin(),
+      scrollbar,
+    ],
+  }
