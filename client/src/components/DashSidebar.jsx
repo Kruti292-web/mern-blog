753cb22988c829d3ae67701dad92a6393a0e7949
Kruti292-web <krutisaliya02@gmail.com>
2024-09-19 20:52:11 +0530

    show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,7 +2,7 @@ import { Sidebar } from "flowbite-react";
 
 import { useEffect, useState } from "react";
 
-import { HiArrowSmRight, HiUser, HiDocumentText } from "react-icons/hi";
+import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
 import { useDispatch } from "react-redux";
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from "react-router-dom";
@@ -60,6 +60,17 @@ export default function DashSidebar() {
                 as='div'
               >
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+            {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item
+                active={tab === 'users'}
+                icon={HiOutlineUserGroup}
+                as='div'
+              >
+                Users
               </Sidebar.Item>
             </Link>
           )}
