97505230b95817ba1102229658f03442857c9a56
Kruti292-web <krutisaliya02@gmail.com>
2024-10-15 20:30:28 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,39 +6,43 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO)
-.then(()=> {
-    console.log('MongoDB is Connected')
-})
-.catch((err)=>{
-    console.log(err)
-});
-
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const __dirname = path.resolve();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes);
-
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}!`)
-})
+  console.log(`Server is running on port ${port}!`);
+});
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
